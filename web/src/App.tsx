1bfd61405efa2bfb53967b8a65c4aa76ac7a8be0
stephen johnson <stephen@sjoh.net>
2019-10-23 11:41:34 -0700

    Add a token sheet you can drag tokens from to place

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,18 +3,16 @@ import Token from "./Token";
 import dwarf from "./dwarf.svg";
 import bear from "./bear.svg";
 import { List } from "immutable";
-import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@material-ui/lab";
 import { makeStyles } from "@material-ui/core";
-import AdbIcon from "@material-ui/icons/Adb";
-import AccessibleForwardIcon from "@material-ui/icons/AccessibleForward";
-import {GRID_SIZE_PX} from "./config";
+import { GRID_SIZE_PX } from "./config";
+import TokenSheet from "./TokenSheet";
 
 let BACKGROUND_COLOR = "#F5F5DC";
 let GRID_COLOR = "#947C65";
 
 const snapToGrid = (x: number) => Math.round(x / GRID_SIZE_PX) * GRID_SIZE_PX;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   map: {
     backgroundColor: BACKGROUND_COLOR,
     backgroundImage: `repeating-linear-gradient(
@@ -34,23 +32,27 @@ const useStyles = makeStyles(theme => ({
     backgroundSize: `${GRID_SIZE_PX}px ${GRID_SIZE_PX}px`,
     height: "100%",
     width: "100%",
-    position: "absolute"
-  },
-  speedDial: {
     position: "absolute",
-    bottom: theme.spacing(2),
-    right: theme.spacing(2)
+    // All the tokens inside the map have to be position absolute so that the
+    // drag offset calculations work properly
+    "& div": {
+      position: "absolute"
+    }
   }
-}));
+});
+
+const TOKEN_TYPES = [
+  { id: "a511ebd2-827b-490d-b20a-c206e4edd25e", icon: bear },
+  { id: "643c7cf8-befb-4a72-b707-9c0399d2a365", icon: dwarf }
+];
 
 const App = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
   const [tokens, setTokens] = useState(
     List.of(
       { x: 0, y: 0, icon: bear },
-      { x: 50, y: 0, icon: dwarf },
-      { x: 100, y: 0, icon: dwarf }
+      { x: 50, y: 50, icon: dwarf },
+      { x: 100, y: 100, icon: dwarf }
     )
   );
 
@@ -59,11 +61,10 @@ const App = () => {
       <div className={classes.map}>
         {tokens.map((token, i) => (
           <Token
-            x={token.x}
-            y={token.y}
+            pos={{ x: token.x, y: token.y }}
             icon={token.icon}
             key={i}
-            onMoved={(x, y) => {
+            onDropped={(x, y) => {
               setTokens(
                 tokens.set(i, {
                   x: snapToGrid(x),
@@ -75,20 +76,18 @@ const App = () => {
           />
         ))}
       </div>
-      <SpeedDial
-        className={classes.speedDial}
-        ariaLabel="Add token"
-        open={open}
-        onOpen={() => setOpen(true)}
-        onClose={() => setOpen(false)}
-        icon={<SpeedDialIcon />}
-      >
-        <SpeedDialAction icon={<AdbIcon />} tooltipTitle="Add bear" />
-        <SpeedDialAction
-          icon={<AccessibleForwardIcon />}
-          tooltipTitle="Add dwarf"
-        />
-      </SpeedDial>
+      <TokenSheet
+        tokenTypes={TOKEN_TYPES}
+        onTokenPlaced={(type, x, y) => {
+          setTokens(
+            tokens.push({
+              x: snapToGrid(x),
+              y: snapToGrid(y),
+              icon: type.icon
+            })
+          );
+        }}
+      />
     </div>
   );
 };
