6609cd43de3e019abab762a6407463e8d1071224
stephen johnson <stephen.johnson@rearden.com>
2019-10-24 15:17:01 -0700

    Moved token management thread into websocket thread
    Tokens are now validated (a little) and actions were added to messages:
    deleting tokens is now possible.

--- api/src/wsmanager.py
+++ api/src/wsmanager.py
@@ -1,8 +1,17 @@
 import asyncio
 import queue
-import socket
+import json
 
 import websockets
+
+
+class RoomData:
+
+    def __init__(self, unique_id):
+
+        self.unique_id = unique_id
+        self.clients = []
+        self.state = {}
 
 
 class WebsocketManager:
@@ -15,6 +24,8 @@ class WebsocketManager:
         self._q = queue.Queue()
         self.host = host
         self.port = port
+        self.rooms = {}
+        self.tokens = {}
         self.send_q = send_q
         self.receive_q = receive_q
 
@@ -38,6 +49,7 @@ class WebsocketManager:
 
         self._connections.add(websocket)
         print(path)
+        self.send_q.put(self.get_state())
         try:
             async for message in websocket:
                 await self.consume(message)
@@ -58,10 +70,42 @@ class WebsocketManager:
 
     async def consume(self, json_message):
 
-        self.receive_q.put(json_message)
+        try:
+            message = json.loads(json_message)
+        except json.JSONDecodeError as e:
+            print(e)
+            return
+        action = message.get('action', None)
+        data = message.get('data', None)
+        if action == 'delete':
+            if self.tokens.get(data['id'], None):
+                del self.tokens[data['id']]
+        elif self.validate_token(data) and action == 'create' or action == 'update':
+            self.create_or_update_token(data)
+        else:
+            print(f'Received an invalid action or token: {action}: {data}')
+        self.send_q.put(self.get_state())
+
+    @staticmethod
+    def validate_token(token):
+
+        return token.get('id', None) and \
+               token.get('x', None) and \
+               token.get('y', None) and \
+               token.get('icon', None)
+
+    def create_or_update_token(self, new_token):
+
+        if new_token and new_token.get('id', None):
+            self.tokens[new_token['id']] = new_token
+            print(new_token)
+
+    def get_state(self):
+
+        return json.dumps(list(self.tokens.values()))
 
 
-def main(send_q, receive_q, host_ip, host_port):
+def start_websocket(send_q, receive_q, host_ip, host_port):
 
     ws = WebsocketManager(send_q, receive_q, host_ip, host_port)
     ws.start_server()
