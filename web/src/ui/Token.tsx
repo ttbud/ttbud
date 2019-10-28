d3d17a7e6ee35b6eb7f88d3bba7c924768b2bdbc
stephen johnson <stephen@sjoh.net>
2019-10-27 22:15:10 -0700

    Remove unused import

--- web/src/ui/Token.tsx
+++ web/src/ui/Token.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react";
 import Draggable from "react-draggable";
 import { Card, CardMedia, makeStyles } from "@material-ui/core";
 import { GRID_SIZE_PX } from "../config";
-import { CardProps } from "@material-ui/core/Card";
 
 const PADDING = GRID_SIZE_PX / 5;
 const CARD_SIZE = GRID_SIZE_PX - PADDING * 2;
