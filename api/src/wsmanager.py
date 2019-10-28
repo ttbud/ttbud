fb9d7a3f006a81655baf5a1984a8f4557ced554c
stephen johnson <stephen.johnson@rearden.com>
2019-10-27 17:00:50 -0700

    Cleaned up queues, fixed bug in room mechanism

--- api/src/wsmanager.py
+++ api/src/wsmanager.py
@@ -7,17 +7,17 @@ import websockets
 
 class WebsocketManager:
 
-    def __init__(self, send_q, receive_q, host, port):
+    def __init__(self, uuid_q, host, port):
 
         self._loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self._loop)
-        self._connections = set()
-        self._q = queue.Queue()
+        self._connections = {}
+        self._send_q = queue.Queue()
         self.host = host
         self.port = port
         self.rooms = {}
-        self.send_q = send_q
-        self.receive_q = receive_q
+        self.uuid_q = uuid_q
+        self._valid_paths = set()
 
     def start_server(self):
 
@@ -37,32 +37,40 @@ class WebsocketManager:
 
     async def consumer_handler(self, websocket, path):
 
-        self._connections.add(websocket)
-        print(path)
-
         p = path.lstrip('/')
-        if self.rooms.get(p, None):
-            self.send_q.put(self.get_state(p))
-        else:
-            self.rooms[p] = {}
-
-        try:
-            async for message in websocket:
-                await self.consume(message, p)
-        finally:
-            self._connections.remove(websocket)
+        print(p)
+        while True:
+            try:
+                self._valid_paths.add(str(self.uuid_q.get_nowait()))
+            except queue.Empty:
+                break
+        if p in self._valid_paths:
+            if self._connections.get(p, None):
+                self._connections[p].add(websocket)
+            else:
+                self._connections[p] = set()
+                self._connections[p].add(websocket)
+            if self.rooms.get(p, None):
+                self._send_q.put((self.get_state(p), p))
+            else:
+                self.rooms[p] = {}
+            try:
+                async for message in websocket:
+                    await self.consume(message, p)
+            finally:
+                self._connections[p].remove(websocket)
 
     async def producer_handler(self):
 
         while True:
             try:
-                message = self.send_q.get_nowait()
+                message, path = self._send_q.get_nowait()
             except queue.Empty:
                 await asyncio.sleep(0.0001)          # Induced delay to free up event loop
                 continue
-            if message is not None and self._connections:
+            if message is not None and self._connections[path]:
                 print(message)
-                await asyncio.wait([client.send(message) for client in self._connections])
+                await asyncio.wait([client.send(message) for client in self._connections[path]])
 
     async def consume(self, json_message, path):
 
@@ -82,15 +90,14 @@ class WebsocketManager:
             self.create_or_update_token(data, path)
         else:
             print(f'Received an invalid action or token: {action}: {data}')
-        self.send_q.put(self.get_state(path))
+        self._send_q.put((self.get_state(path), path))
 
     def validate_token(self, token):
 
-        return token.get('id', False) and \
-               token.get('x', False) and \
-               token.get('y', False) and \
-               token.get('icon', False) and \
-               self.validate_position(token['x'], token['y'])
+        return 'id' in token.keys() and \
+               'x' in token.keys() and \
+               'y' in token.keys() and \
+               'icon' in token.keys()
 
     def validate_position(self, new_token, path):
 
@@ -110,7 +117,7 @@ class WebsocketManager:
         return json.dumps(list(self.rooms[path].values()))
 
 
-def start_websocket(send_q, receive_q, host_ip, host_port):
+def start_websocket(uuid_q, host_ip, host_port):
 
-    ws = WebsocketManager(send_q, receive_q, host_ip, host_port)
+    ws = WebsocketManager(uuid_q, host_ip, host_port)
     ws.start_server()
