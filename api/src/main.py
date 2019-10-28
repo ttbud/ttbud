3ed5f282fabb77e9c6e17b64b47701ca0942629c
stephen johnson <stephen.johnson@rearden.com>
2019-10-27 17:00:50 -0700

    Cleaned up queues, fixed bug in room mechanism

--- api/src/main.py
+++ api/src/main.py
@@ -6,7 +6,7 @@ import socket
 import flask
 from flask_cors import CORS
 
-from api.src.wsmanager import WebsocketManager, start_websocket
+from api.src.wsmanager import start_websocket
 
 
 if __name__ == '__main__':
@@ -16,10 +16,9 @@ if __name__ == '__main__':
     host = socket.gethostbyname(socket.gethostname())
     websocket_port = 8765
     tokens = {}
-    send_q = queue.Queue()
-    receive_q = queue.Queue()
+    uuid_q = queue.Queue()
 
-    ws = threading.Thread(target=start_websocket, args=(send_q, receive_q, host, websocket_port))
+    ws = threading.Thread(target=start_websocket, args=(uuid_q, host, websocket_port))
     ws.daemon = True
     ws.start()
 
@@ -27,6 +26,8 @@ if __name__ == '__main__':
     def create_websocket_session():
 
         new_id = uuid.uuid4()
+        print(f'New UUID: {new_id}')
+        uuid_q.put(new_id)
         return flask.make_response({'path': f'ws://{host}:{websocket_port}/{new_id}'})
 
     app.run(host='0.0.0.0', port=5000)
