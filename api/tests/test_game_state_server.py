62bc063543a1f8375c7cf698fcf8eb37455d2389
Stephen Johnson <github@sjoh.net>
2020-01-24 18:44:19 -0800

    Also change the names in the tests that are about to be deleted

--- api/tests/test_game_state_server.py
+++ api/tests/test_game_state_server.py
@@ -15,21 +15,21 @@ test_token = {
 
 def test_validate_token():
     valid_token = gss.Token(**test_token)
-    assert gss.GameStateServer.is_valid_token(valid_token) is True
+    assert gss.GameStateServer._is_valid_token(valid_token) is True
 
     invalid_token = gss.Token(**test_token)
     invalid_token.start_x = 2
-    assert gss.GameStateServer.is_valid_token(invalid_token) is False
+    assert gss.GameStateServer._is_valid_token(invalid_token) is False
 
 
 def test_get_unit_blocks():
     valid_token = gss.Token(**test_token)
-    assert gss.GameStateServer.get_unit_blocks(valid_token) == [(0, 0, 0)]
+    assert gss.GameStateServer._get_unit_blocks(valid_token) == [(0, 0, 0)]
 
     valid_token.end_x = 2
     valid_token.end_y = 2
     valid_token.end_z = 2
-    blocks = gss.GameStateServer.get_unit_blocks(valid_token)
+    blocks = gss.GameStateServer._get_unit_blocks(valid_token)
     assert (0, 0, 0) in blocks
     assert (1, 0, 0) in blocks
     assert (0, 1, 0) in blocks
