6454b678c0e83148fb1828ee79d57ede475431bf
Stephen Johnson <github@sjoh.net>
2020-01-24 18:29:32 -0800

    Made some methods private, plus one more dict return obliterated

--- api/src/game_state_server.py
+++ api/src/game_state_server.py
@@ -27,8 +27,8 @@ class RoomData:
     def __init__(self, room_id: str, initial_connection=None):
         self.room_id = room_id
         self.game_state = {}
-        self.id_to_positions = {}
-        self.positions_to_ids = {}
+        self._id_to_positions = {}
+        self._positions_to_ids = {}
         self.clients = set()
         if initial_connection:
             self.clients.add(initial_connection)
@@ -49,14 +49,14 @@ class GameStateServer:
 
     def new_connection_request(
         self, client: WebSocketServerProtocol, room_id: str
-    ) -> dict:
+    ) -> Reply:
         if self._rooms.get(room_id, False):
             self._rooms[room_id].clients.add(client)
         else:
             self._rooms[room_id] = RoomData(room_id, initial_connection=client)
             if self.rs.room_data_exists(room_id):
                 self._rooms[room_id].game_state = self.rs.read_room_data(room_id)
-        return {'type': 'state', 'data': self.get_state(room_id)}
+        return Reply('state', self.get_state(room_id))
 
     def connection_dropped(self, client: WebSocketServerProtocol, room_id: str) -> None:
         if self._rooms.get(room_id, False):
@@ -76,18 +76,18 @@ class GameStateServer:
             raise MessageError('Did not receive a full message')
 
         if action == 'create' or action == 'update':
-            token = self.dict_to_token(data)
-            if not token or not self.is_valid_token(token):
+            token = self._dict_to_token(data)
+            if not token or not self._is_valid_token(token):
                 raise MessageError(f'Received a bad token {data}')
-            if not self.is_valid_position(token, room_id):
+            if not self._is_valid_position(token, room_id):
                 raise MessageError('That position is occupied, bucko')
-            self.create_or_update_token(token, room_id)
+            self._create_or_update_token(token, room_id)
             return Reply('state', self.get_state(room_id))
         elif action == 'delete':
             if type(data) != str:
                 raise MessageError('Data for delete actions must be a token ID')
             if self._rooms[room_id].game_state.get(data, False):
-                self.delete_token(data, room_id)
+                self._delete_token(data, room_id)
                 return Reply('state', self.get_state(room_id))
             else:
                 raise MessageError('Cannot delete token because it does not exist')
@@ -97,7 +97,7 @@ class GameStateServer:
             raise MessageError(f'Invalid action: {action}')
 
     @staticmethod
-    def dict_to_token(data: dict):
+    def _dict_to_token(data: dict):
         try:
             token = Token(**data)
         except TypeError:
@@ -105,49 +105,49 @@ class GameStateServer:
         return token
 
     @staticmethod
-    def is_valid_token(token: Token) -> bool:
+    def _is_valid_token(token: Token) -> bool:
         return (
             token.start_x < token.end_x
             and token.start_y < token.end_y
             and token.start_z < token.end_z
         )
 
-    def is_valid_position(self, token: Token, room_id: str) -> bool:
-        blocks = self.get_unit_blocks(token)
+    def _is_valid_position(self, token: Token, room_id: str) -> bool:
+        blocks = self._get_unit_blocks(token)
         for block in blocks:
             if self._rooms[room_id].positions_to_ids.get(block, False):
                 return False
         return True
 
-    def create_or_update_token(self, token: Token, room_id: str) -> None:
+    def _create_or_update_token(self, token: Token, room_id: str) -> None:
         print(f'New token: {token}')
         if self._rooms[room_id].game_state.get(token.id):
-            self.remove_positions(token.id, room_id)
+            self._remove_positions(token.id, room_id)
 
         # Update state for new or existing token
-        blocks = self.get_unit_blocks(token)
+        blocks = self._get_unit_blocks(token)
         self._rooms[room_id].id_to_positions[token.id] = blocks
         for block in blocks:
             self._rooms[room_id].positions_to_ids[block] = token.id
         self._rooms[room_id].game_state[token.id] = asdict(token)
 
-    def delete_token(self, token_id: str, room_id: str) -> None:
+    def _delete_token(self, token_id: str, room_id: str) -> None:
         # Remove token data from position dictionaries
-        self.remove_positions(token_id, room_id)
+        self._remove_positions(token_id, room_id)
         if self._rooms[room_id].id_to_positions.get(token_id, False):
             del self._rooms[room_id].id_to_positions[token_id]
         # Remove the token from the state
         if self._rooms[room_id].game_state.get(token_id, False):
             del self._rooms[room_id].game_state[token_id]
 
-    def remove_positions(self, token_id: str, room_id: str) -> None:
+    def _remove_positions(self, token_id: str, room_id: str) -> None:
         positions = self._rooms[room_id].id_to_positions[token_id]
         for pos in positions:
             if self._rooms[room_id].positions_to_ids.get(pos, False):
                 del self._rooms[room_id].positions_to_ids[pos]
 
     @staticmethod
-    def get_unit_blocks(token: Token) -> list:
+    def _get_unit_blocks(token: Token) -> list:
         unit_blocks = []
         for x in range(token.start_x, token.end_x):
             for y in range(token.start_y, token.end_y):
