6d613f98c25bc2e9af63466c191ec19a538c52e9
Stephen Johnson <github@sjoh.net>
2020-01-24 21:32:07 -0800

    Remove old tests and add a new, shinier test

--- api/tests/test_game_state_server.py
+++ api/tests/test_game_state_server.py
@@ -1,40 +1,20 @@
-import game_state_server as gss
+from game_state_server import GameStateServer, Token
+from room_store import RoomStore
 
 
-test_token = {
-    'id': 'valid-id',
-    'icon_id': 'valid_icon_id',
-    'start_x': 0,
-    'start_y': 0,
-    'start_z': 0,
-    'end_x': 1,
-    'end_y': 1,
-    'end_z': 1,
-}
+class FakeRoomStore(RoomStore):
+    # Don't call super constructor so we don't actually make the directory
+    # noinspection PyMissingConstructor
+    def __init__(self, room_store_dir):
+        self.path = room_store_dir
+
+    def get_all_room_ids(self) -> list:
+        return []
 
 
-def test_validate_token():
-    valid_token = gss.Token(**test_token)
-    assert gss.GameStateServer._is_valid_token(valid_token) is True
-
-    invalid_token = gss.Token(**test_token)
-    invalid_token.start_x = 2
-    assert gss.GameStateServer._is_valid_token(invalid_token) is False
-
-
-def test_get_unit_blocks():
-    valid_token = gss.Token(**test_token)
-    assert gss.GameStateServer._get_unit_blocks(valid_token) == [(0, 0, 0)]
-
-    valid_token.end_x = 2
-    valid_token.end_y = 2
-    valid_token.end_z = 2
-    blocks = gss.GameStateServer._get_unit_blocks(valid_token)
-    assert (0, 0, 0) in blocks
-    assert (1, 0, 0) in blocks
-    assert (0, 1, 0) in blocks
-    assert (0, 0, 1) in blocks
-    assert (1, 1, 0) in blocks
-    assert (1, 0, 1) in blocks
-    assert (0, 1, 1) in blocks
-    assert (1, 1, 1) in blocks
+def test_new_connection():
+    rs = FakeRoomStore('/my/path/to/rooms/')
+    gss = GameStateServer(rs)
+    reply = gss.new_connection_request('test_client', 'room1')
+    assert reply.type == 'state'
+    assert len(reply.data) == 0
