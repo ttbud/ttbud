e1f87e40ba135c732a87f468a37b74cf96877899
Stephen Johnson <github@sjoh.net>
2020-01-26 15:52:53 -0800

    Directly use fixtures without assigning them to a local variable

--- api/tests/test_game_state_server.py
+++ api/tests/test_game_state_server.py
@@ -20,66 +20,59 @@ valid_update = {'action': 'create', 'data': valid_data}
 
 
 @pytest.fixture
-def set_up_gss():
+def gss():
     rs = MemoryRoomStore('/my/path/to/room/storage/')
     return GameStateServer(rs)
 
 
 @pytest.fixture
-def set_up_gss_with_client():
+def gss_with_client():
     rs = MemoryRoomStore('/my/path/to/room/storage/')
     gss = GameStateServer(rs)
     gss.new_connection_request(TEST_CLIENT, TEST_ROOM_ID)
     return gss
 
 
-def test_new_connection(set_up_gss):
-    gss = set_up_gss
+def test_new_connection(gss):
     reply = gss.new_connection_request('test_client', 'room1')
     assert reply.type == 'state'
     assert len(reply.data) == 0
 
 
-def test_room_does_not_exist(set_up_gss):
-    gss = set_up_gss
+def test_room_does_not_exist(gss):
     with pytest.raises(MessageError):
         gss.process_update({}, 'room id that does not exist')
 
 
-def test_room_data_is_stored(set_up_gss_with_client):
-    gss = set_up_gss_with_client
-    gss.process_update(valid_update, TEST_ROOM_ID)
-    gss.connection_dropped(TEST_CLIENT, TEST_ROOM_ID)
-    assert gss.room_store.stored_data.get(TEST_ROOM_ID, False)
-    assert gss.room_store.stored_data[TEST_ROOM_ID].get(valid_data['id'], False)
-    assert gss.room_store.stored_data[TEST_ROOM_ID][valid_data['id']] == valid_data
+def test_room_data_is_stored(gss_with_client):
+    gss_with_client.process_update(valid_update, TEST_ROOM_ID)
+    gss_with_client.connection_dropped(TEST_CLIENT, TEST_ROOM_ID)
+    assert gss_with_client.room_store.stored_data.get(TEST_ROOM_ID, False)
+    assert gss_with_client.room_store.stored_data[TEST_ROOM_ID].get(valid_data['id'], False)
+    assert gss_with_client.room_store.stored_data[TEST_ROOM_ID][valid_data['id']] == valid_data
 
 
-def test_duplicate_update_rejected(set_up_gss_with_client):
-    gss = set_up_gss_with_client
-    gss.process_update(valid_update, TEST_ROOM_ID)
+def test_duplicate_update_rejected(gss_with_client):
+    gss_with_client.process_update(valid_update, TEST_ROOM_ID)
     with pytest.raises(MessageError):
-        gss.process_update(valid_update, TEST_ROOM_ID)
+        gss_with_client.process_update(valid_update, TEST_ROOM_ID)
 
 
-def test_duplicate_update_in_different_room(set_up_gss):
-    gss = set_up_gss
+def test_duplicate_update_in_different_room(gss):
     gss.new_connection_request('client1', 'room1')
     gss.new_connection_request('client2', 'room2')
     gss.process_update(valid_update, 'room1')
     gss.process_update(valid_update, 'room2')
 
 
-def test_delete_token(set_up_gss_with_client):
-    gss = set_up_gss_with_client
-    gss.process_update(valid_update, TEST_ROOM_ID)
-    reply = gss.process_update(
+def test_delete_token(gss_with_client):
+    gss_with_client.process_update(valid_update, TEST_ROOM_ID)
+    reply = gss_with_client.process_update(
         {'action': 'delete', 'data': valid_data['id']}, TEST_ROOM_ID
     )
     assert len(reply.data) == 0
 
 
-def test_delete_non_existent_token(set_up_gss_with_client):
-    gss = set_up_gss_with_client
+def test_delete_non_existent_token(gss_with_client):
     with pytest.raises(MessageError):
-        gss.process_update({'action': 'delete', 'data': valid_data['id']}, TEST_ROOM_ID)
+        gss_with_client.process_update({'action': 'delete', 'data': valid_data['id']}, TEST_ROOM_ID)
