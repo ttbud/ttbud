a4971072cb99710c72d88cc6245e6605f025d9ed
Stephen Johnson <github@sjoh.net>
2020-01-26 12:53:17 -0800

    Restructure room store classes to inherit from a Protocol

--- api/src/room_store.py
+++ api/src/room_store.py
@@ -1,10 +1,32 @@
 import os
 import json
+from typing import Protocol
+from abc import abstractmethod
 
 
-class RoomStore:
-    def __init__(self, room_store_dir):
-        self.path = room_store_dir
+class RoomStore(Protocol):
+    path: str
+
+    @abstractmethod
+    def get_all_room_ids(self) -> list:
+        raise NotImplementedError
+
+    @abstractmethod
+    def write_room_data(self, room_id: str, data: dict) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def read_room_data(self, room_id: str) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def room_data_exists(self, room_id: str) -> bool:
+        raise NotImplementedError
+
+
+class FileRoomStore(RoomStore):
+    def __init__(self, path):
+        self.path = path
         os.makedirs(self.path, exist_ok=True)
 
     def get_all_room_ids(self) -> list:
@@ -13,7 +35,7 @@ class RoomStore:
     def _is_valid_path(self, full_path: str) -> bool:
         return os.path.abspath(full_path).startswith(self.path)
 
-    def write_room_data(self, room_id: str, data: dict):
+    def write_room_data(self, room_id: str, data: dict) -> None:
         full_path = f'{self.path}/{room_id}'
         if self._is_valid_path(full_path):
             with open(full_path, 'w') as f:
@@ -27,3 +49,21 @@ class RoomStore:
 
     def room_data_exists(self, room_id: str) -> bool:
         return os.path.exists(f'{self.path}/{room_id}')
+
+
+class MemoryRoomStore(RoomStore):
+    def __init__(self, path):
+        self.path = path
+        self.stored_data = {}
+
+    def get_all_room_ids(self) -> list:
+        return list(self.stored_data.keys())
+
+    def write_room_data(self, room_id: str, data: dict):
+        self.stored_data[room_id] = data
+
+    def read_room_data(self, room_id: str) -> dict:
+        return self.stored_data[room_id]
+
+    def room_data_exists(self, room_id: str) -> bool:
+        return room_id in self.stored_data.keys()
