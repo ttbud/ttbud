ff768f98aca833feb50f08c6428c05cc268160e9
Stephen Johnson <github@sjoh.net>
2020-01-26 14:45:51 -0800

    More unit tests for game state server

--- api/tests/test_game_state_server.py
+++ api/tests/test_game_state_server.py
@@ -1,7 +1,18 @@
 import pytest
 
-from game_state_server import GameStateServer, MessageError
+from game_state_server import GameStateServer, MessageError, Token
 from room_store import MemoryRoomStore
+
+
+valid_data = {'id': 'some_id',
+              'icon_id': 'some_icon_id',
+              'start_x': 0,
+              'start_y': 0,
+              'start_z': 0,
+              'end_x': 1,
+              'end_y': 1,
+              'end_z': 1,
+              }
 
 
 def test_new_connection():
@@ -17,3 +28,17 @@ def test_room_does_not_exist():
     gss = GameStateServer(rs)
     with pytest.raises(MessageError):
         gss.process_update({}, 'room id that does not exist')
+
+
+def test_room_data_is_stored():
+    rs = MemoryRoomStore('/my/path/to/rooms/')
+    gss = GameStateServer(rs)
+    gss.new_connection_request('client1', 'room1')
+    gss.process_update({
+        'action': 'create',
+        'data': valid_data,
+    }, 'room1')
+    gss.connection_dropped('client1', 'room1')
+    assert gss.room_store.stored_data.get('room1', False)
+    assert gss.room_store.stored_data['room1'].get(valid_data['id'], False)
+    assert gss.room_store.stored_data['room1'][valid_data['id']] == valid_data
