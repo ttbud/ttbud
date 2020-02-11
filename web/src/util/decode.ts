969d27f7276407b6e45255e4ca8ddbe30f19608f
Sam Burba <github@samburba.com>
2020-02-11 13:54:01 -0500

    Add docs to decode

--- web/src/util/decode.ts
+++ web/src/util/decode.ts
@@ -1,6 +1,9 @@
 import * as t from "io-ts";
 import { PathReporter } from "io-ts/lib/PathReporter";
 
+/**
+ * Decode input using the provided validator, throwing an exception on error
+ */
 export default function decode<T, O, I>(validator: t.Type<T, O, I>, input: I): T {
   const result = validator.decode(input);
   if (result._tag === "Left") {
