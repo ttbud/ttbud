dee9aeeaf489102e0b76120b83eb2654f03d7bf7
Sam Burba <github@samburba.com>
2020-02-12 22:08:01 -0500

    style cleanup

--- web/src/util/decode.ts
+++ web/src/util/decode.ts
@@ -4,7 +4,10 @@ import { PathReporter } from "io-ts/lib/PathReporter";
 /**
  * Decode input using the provided validator, throwing an exception on error
  */
-export default function decode<T, O, I>(validator: t.Type<T, O, I>, input: I): T {
+export default function decode<T, O, I>(
+  validator: t.Type<T, O, I>,
+  input: I
+): T {
   const result = validator.decode(input);
   if (result._tag === "Left") {
     throw new Error(PathReporter.report(result).join("\n"));
