12d643a5db88d2805bc5961959da0c295f87c69a
Sam Burba <github@samburba.com>
2020-02-21 17:45:39 -0500

    Remove unused timeout function

--- web/src/util/timeout.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Create a promise that resolves in timeMs milliseconds
- */
-export default function timeout(timeMs: number) {
-  return new Promise(resolve => {
-    window.setTimeout(resolve, timeMs);
-  });
-}
