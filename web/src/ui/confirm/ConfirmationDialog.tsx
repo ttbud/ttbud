a6fcfa06c5065bfea90ec1105f739cd38fb3ffb2
Sam Burba <github@samburba.com>
2020-03-28 19:01:09 -0700

    Add a clear room and new room button

---
+++ web/src/ui/confirm/ConfirmationDialog.tsx
@@ -0,0 +1,53 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@material-ui/core";
+import React from "react";
+
+interface Props {
+  open: boolean;
+  title: string;
+  description: string;
+  confirmAction: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmationDialog: React.FC<Props> = ({
+  open,
+  title,
+  description,
+  confirmAction,
+  onConfirm,
+  onCancel,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={onCancel}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {description}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={onConfirm} color="primary" autoFocus>
+          {confirmAction}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
