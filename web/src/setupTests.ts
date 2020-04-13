4bf7fe40396ed59a52307e567211fc08c2571dcd
Sam Burba <github@samburba.com>
2020-04-13 10:22:20 -0700

    Mock out scrollTo in tests
    jsdom doesn't support scrollTo

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -1,5 +1,9 @@
 jest.mock("./ui/icon-loader.ts");
 
+// Mock out scrollTo since jsdom doesn't support it
+// @ts-ignore
+global.scrollTo = jest.fn();
+
 // Useless export to convince typescript that this is a module
 // https://stackoverflow.com/questions/56577201/why-is-isolatedmodules-error-fixed-by-any-import/56577324
 export {};
