1c04648dd5c1e6f2813499d4ba478ad65b603cc3
Sam Burba <github@samburba.com>
2020-05-21 21:09:41 -0700

    Don't create empty text tokens

--- web/src/ui/search/SearchDialog.tsx
+++ web/src/ui/search/SearchDialog.tsx
@@ -78,7 +78,7 @@ const SearchDialog: React.FC<Props> = memo(({ icons, open, onClose }) => {
   }, [search, items]);
 
   const textContents: TokenContents | undefined =
-    search.length <= 2 ? { type: ContentType.Text, text: search } : undefined;
+    search.length > 0 && search.length <= 2 ? { type: ContentType.Text, text: search } : undefined;
 
   const textItem: DraggableDescriptor | undefined = !textContents
     ? undefined
