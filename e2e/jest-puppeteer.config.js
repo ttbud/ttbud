698f7acd855a96d833941cfa932d870bf9a84605
Sam Burba <github@samburba.com>
2020-05-22 03:58:04 -0700

    Increase element wait timeout
    Add a debug mode environment variable

--- e2e/jest-puppeteer.config.js
+++ e2e/jest-puppeteer.config.js
@@ -1,21 +1,23 @@
-const args =
-  process.env.IN_DOCKER !== "true"
-    ? []
-    : [
-        // Disabling the sandbox is required in the circleci docker environment
-        // This is scary but okay because we're only ever navigating to content
-        // on our website, which we trust
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        // This will write shared memory files into /tmp instead of /dev/shm,
-        // because Docker’s default for /dev/shm is 64MB
-        "--disable-dev-shm-usage",
-      ];
+const config = require("./config");
+
+const args = config.ci
+  ? []
+  : [
+      // Disabling the sandbox is required in the circleci docker environment
+      // This is scary but okay because we're only ever navigating to content
+      // on our website, which we trust
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      // This will write shared memory files into /tmp instead of /dev/shm,
+      // because Docker’s default for /dev/shm is 64MB
+      "--disable-dev-shm-usage",
+    ];
 
 module.exports = {
   launch: {
     dumpio: true,
-    headless: process.env.HEADLESS !== "false",
+    headless: !config.debugMode,
+    slowMo: config.debugMode ? 250 : undefined,
     ignoreHTTPSErrors: true,
     args,
   },
