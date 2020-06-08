ba89fba3cc0667b64ea411374c74c78712ab35f3
Sam Burba <github@samburba.com>
2020-06-07 20:26:52 -0700

    Clean up request validation (#424)
    * Clean up request validation
    
    Use dataclasseses for the full request data, use dacite to validate them
    instead of ad-hoc validation.
    
    * Clean up request validation
    
    Use dataclasseses for the full request data, use dacite to validate them
    instead of ad-hoc validation.
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Add documentation to assert_never.py
    
    * Update api/tests/test_game_state_server.py
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>
    
    * Remove unused imports
    
    Co-authored-by: Stephen Johnson <github@sjoh.net>

---
+++ api/src/assert_never.py
@@ -0,0 +1,9 @@
+from typing import NoReturn
+
+
+def assert_never(x: NoReturn) -> NoReturn:
+    """This function tells mypy that the branch that called it should never execute
+    It can be used to tell mypy to force that branches before this must cover all cases
+    (Such as checking all values in an enum or a discriminated union)
+    """
+    assert False, f'Unhandled type: {type(x)}'
