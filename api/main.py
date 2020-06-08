1010e56fcac72badbe84c551025ac641fa3f8e6d
Stephen Johnson <github@sjoh.net>
2020-06-07 18:44:57 -0700

    Rename functions

--- api/main.py
+++ api/main.py
@@ -11,11 +11,11 @@ from src.room_store import DatabaseRoomStore
 from src.game_state_server import GameStateServer
 
 
-async def make_objects():
+async def start_server():
     room_store = await DatabaseRoomStore.obtain(config.db_address)
     gss = GameStateServer(room_store)
     ws = WebsocketManager(config.websocket_port, gss)
-    await ws.start_server()
+    await ws.start_websocket()
     return gss
 
 
@@ -24,7 +24,7 @@ def main():
     scout_apm.api.install(config=config.scout_config)
 
     loop = asyncio.get_event_loop()
-    gss = loop.run_until_complete(make_objects())
+    gss = loop.run_until_complete(start_server())
     loop.add_signal_handler(
         signal.SIGTERM, lambda *_: loop.run_until_complete(gss.save_all())
     )
