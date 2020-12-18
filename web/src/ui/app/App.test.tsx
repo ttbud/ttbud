453cc901012479489de622efca8ec6fdc0c5a3be
Sam Burba <github@samburba.com>
2020-12-18 15:22:25 -0800

    Properly handle update rejections
    Fixes #499

--- web/src/ui/app/App.test.tsx
+++ web/src/ui/app/App.test.tsx
@@ -5,13 +5,13 @@ import { DomDroppableMonitor } from "../../drag/DroppableMonitor";
 import createStore from "../../store/createStore";
 import { Provider } from "react-redux";
 import DndContext from "../../drag/DndContext";
-import { BoardStateApiClient } from "../../network/BoardStateApiClient";
+import FakeApiClient from "../../network/__test_util__/FakeApiClient";
 
 jest.mock("../../network/BoardStateApiClient");
 
 it("renders without crashing", () => {
   const monitor = new DomDroppableMonitor();
-  const apiClient = new BoardStateApiClient("invalid");
+  const apiClient = new FakeApiClient();
   const store = createStore(monitor, apiClient);
 
   const div = document.createElement("div");
