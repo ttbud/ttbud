f2b638e68106bfa525ab89ac5bd7d599577723f3
Stephen Johnson <github@sjoh.net>
2022-11-22 22:37:04 -0800

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -79,7 +79,7 @@ module "api" {
   organization                     = "ttbud"
   autoidle                         = !local.is_prod
   redis_plan                       = "mini"
-  dyno_size                        = local.is_prod ? "hobby" : "eco"
+  dyno_size                        = "hobby"
   region                           = "us"
   env_name                         = local.short_env
   deploy_tarball_url               = var.api_deploy_tarball_url
