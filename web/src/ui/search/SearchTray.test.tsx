c2fe948cbbca78e139ec22e6e49cea717e078852
Stephen Johnson <github@sjoh.net>
2024-01-05 16:26:24 -0800

    Remove unused deps

--- web/src/ui/search/SearchTray.test.tsx
+++ web/src/ui/search/SearchTray.test.tsx
@@ -1,19 +1,14 @@
 import { render } from "@testing-library/react";
 import SearchTray from "./SearchTray";
 import noop from "../../util/noop";
-import { DEFAULT_FLOOR_ICONS, WALL_ICON } from "../icons";
+import { DEFAULT_FLOOR_ICONS } from "../icons";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import dragReducer from "../../drag/drag-slice";
 import { FakeDroppableMonitor } from "../../drag/__test_util__/FakeDroppableMonitor";
 import { DomDroppableMonitor } from "../../drag/DroppableMonitor";
 import DndContext from "../../drag/DndContext";
-import {
-  DraggableType,
-  DragState,
-  DragStateType,
-} from "../../drag/DragStateTypes";
-import { ContentType } from "../../types";
+import { DragState, DragStateType } from "../../drag/DragStateTypes";
 import { ComponentProps } from "react";
 import userEvent from "@testing-library/user-event";
 
